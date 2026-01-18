f1d7e3971e9ba286add3cef7d20569447592fb69
z-aditya-pradhan <adityap@zenarate.com>
2026-01-18 15:36:31 +0530

    small change added

--- aws.tf
+++ aws.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = var.ASG_Region
   profile = "Aditya-demo"
 }
-resource "aws_ami_from_instance" "uat-autoscaling" {
+resource "aws_ami_from_instance" "autoscaling" {
   name               = "${var.ASG_NAME}-${var.DATE}"
   source_instance_id = "${var.INSTANCE_ID}"
 }
 
 output "autoscaling_id" {
-  value = "${aws_ami_from_instance.uat-autoscaling.id}"
+  value = "${aws_ami_from_instance.autoscaling.id}"
 }
 variable "ASG_Region" {
 }
